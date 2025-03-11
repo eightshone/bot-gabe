498bf514b2c45691ba830fcf1cf897d7610edc98
Eightsh One <eightsh.one@gmail.com>
2025-03-11 09:01:10 +0100

    feat(utils): add support for ip addresses in url validation

--- src/utils/isValidURL.ts
+++ src/utils/isValidURL.ts
@@ -1,4 +1,5 @@
 function isValidURL(url: string): boolean {
-  const pattern = /^(https?):\/\/[^\s/$.?#].[^\s]*$/i;
+  const pattern =
+    /^(https?:\/\/)?((([a-zA-Z\d]([a-zA-Z\d-]*[a-zA-Z\d])*)\.)+[a-zA-Z]{2,}|((\d{1,3}\.){3}\d{1,3}))(:\d+)?(\/[^\s]*)?$/;
   return pattern.test(url);
 }

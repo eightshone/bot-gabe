c8bf214ea19f0f405f5bacc7b8e26b1b0a892a29
Eightsh One <eightsh.one@gmail.com>
2025-03-24 14:03:34 +0100

    feat(cli): add single-url option and integrate sitemap crawling

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ import { VERSION } from "./utils/consts";
 import goodbye from "./utils/goodbye";
 import isValidURL from "./utils/isValidURL";
 import runTest from "./core/functions/runLoadTest";
+import getSiteUrls from "./utils/getSiteUrls";
+import getSitemap from "./utils/getSitemap";
+import cleanupSitemapUrl from "./utils/cleanupSitemapUrl";
+import shuffleArray from "./utils/shuffleArray";
 
 const program = new Command();
 
@@ -13,17 +17,26 @@ program
   .description("A load testing cli tool that simulates bots behavior")
   .version(VERSION, "-v, --version", "output the version number");
 
-program.argument("<url>", "entry URL to crawl").action(app);
+program
+  .argument("<url>", "entry URL to crawl")
+  .option("-s, --single-url", "skips sitemap files and only tests given url")
+  .action(app);
 
 program.parse();
 
-async function app(url: string) {
+async function app(url: string, options) {
   if (!isValidURL(url)) {
     console.log("❌ Invalid URL format");
     return;
   }
 
-  await runTest([url]);
+  await runTest(
+    options.singleUrl
+      ? [url]
+      : shuffleArray(
+          await getSiteUrls(cleanupSitemapUrl(url, await getSitemap(url)), url)
+        )
+  );
 }
 
 process.on("uncaughtException", (error) => {

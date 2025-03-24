c5fa30a93aba3df4e5294dbd231e658bebf3a9a8
Eightsh One <eightsh.one@gmail.com>
2025-03-24 13:34:40 +0100

    feat(utils): add a fallback url if there are no urls in the sitemap

--- src/utils/getSiteUrls.ts
+++ src/utils/getSiteUrls.ts
@@ -1,13 +1,13 @@
 import Sitemapper from "sitemapper";
 
-async function getSiteUrls(sitemapUrl: string) {
+async function getSiteUrls(sitemapUrl: string, fallback: string) {
   const sitemap = new Sitemapper({
     url: sitemapUrl,
     timeout: 15000,
   });
   const sites = await sitemap.fetch();
 
-  return sites?.sites ?? [];
+  return sites?.sites?.length ? sites?.sites : [fallback];
 }
 
 export default getSiteUrls;

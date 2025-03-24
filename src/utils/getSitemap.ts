0028a25b33f0c0fa64a24cc60a93854b789615df
Eightsh One <eightsh.one@gmail.com>
2025-03-24 12:14:39 +0100

    feat(utils): add getSitemap function

---
+++ src/utils/getSitemap.ts
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+const getSitemap = async (url: string) => {
+  let sitemap = "sitemap.xml";
+  const response = await axios.get(`${url}/robots.txt`);
+  if (response?.data) {
+    const lines = response.data.split("\n");
+    for (const line of lines) {
+      const match = line.match(/^Sitemap:\s*(.+)$/i);
+      if (match) {
+        sitemap = match[1];
+        break; // Stops looping once a sitemap is found
+      }
+    }
+  }
+
+  return sitemap;
+};
+
+export default getSitemap;
